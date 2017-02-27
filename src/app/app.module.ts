7072336fe4b33e49e9a66cbc454672d83d552b5a
ewelina <evelinagonda@gmail.com>
2017-02-27 15:06:29 +0100

    init routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './shared/in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import {TodosService} from './shared/todos.service';
+import { TodosService } from './shared/todos.service';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
   providers: [TodosService],
   bootstrap: [AppComponent]
--- src/app/shared/mock-todos.ts
+++
@@ -1,8 +0,0 @@
-import {Todo} from './todo.model';
-
-export const TODOS: Todo[] = [
-    {id:1, text:'sometxt1', state: 'todo'},
-    {id:2, text:'sometxt2', state: 'inProgress'},
-    {id:3, text:'sometxt3', state: 'done'},
-    {id:4, text:'sometxt4', state: 'todo'},
-];
