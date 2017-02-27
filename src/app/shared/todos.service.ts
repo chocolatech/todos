db5b1802c4ffa61d4a20f12690cb4b7488f0fbc5
ewelina <evelinagonda@gmail.com>
2017-02-27 15:06:14 +0100

    add topromise

--- src/app/shared/todos.service.ts
+++ src/app/shared/todos.service.ts
@@ -1,12 +1,27 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
-import {TODOS} from './mock-todos';
+import { Todo } from './todo.model';
 
 @Injectable()
 export class TodosService {
-    getTodos(){
-        return Promise.resolve(TODOS);
+    // getTodos(){
+    //     return Promise.resolve(Todo);
+    // }
+    private todosUrl = 'api/todos';  // URL to web api
+
+    constructor(private http: Http) { }
+
+    getHeroes(): Promise<Todo[]> {
+        return this.http.get(this.todosUrl)
+            .toPromise()
+            .then(response => response.json().data as Todo[])
+            .catch(this.handleError);
     }
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+
 }
